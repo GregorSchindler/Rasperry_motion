6559d03ab1736fa693b4c52de258d8ad925a9104
Gregor Schindler <undeq@student.kit.edu>
2018-12-02 00:16:20 +0100

    play video

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-#import numpy
 import random as rn
-
+import numpy as np
 # import RPi.GPIO as GPIO
 import youtube_dl
 import pafy
 import vlc
+import time
 import numpy
 
-while 1:
-    test = rn.randint(0,1)
-    if test == 1:
-        #mixer.init()
-        #mixer.music.load('boom.wav')
-        #mixer.music.play()
-        url = "https://www.youtube.com/watch?v=WddpRmmAYkg&t=1519s"
-        video = pafy.new(url)
-        best = video.getbest()
-        playurl = best.url
 
-        Instance = vlc.Instance()
-        player = Instance.media_player_new()
-        Media = Instance.media_new(playurl)
-        Media.get_mrl()
-        player.set_media(Media)
-        player.play()
+#while 1:
+test = rn.randint(0,1)
+
+
+url = "https://www.youtube.com/watch?v=WddpRmmAYkg&t=1519s"
+video = pafy.new(url)
+best = video.getbest()
+playurl = best.url
+
+instance = vlc.Instance("--no-xlib")
+player = instance.media_player_new()
+media = instance.media_new(playurl)
+media.get_mrl()
+player.set_media(media)
+player.play()
+time.sleep(15000)
+
+
 
 
