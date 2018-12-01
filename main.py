696ed3cd1755d733737f5df43aa333acbac9e045
Gregor Schindler <undeq@student.kit.edu>
2018-12-01 21:41:50 +0100

    play music

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import numpy
+#import numpy
 import time
 import os
 import random as rn
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+from pygame import mixer
+import playsound
 
 
 while 1:
-    test == rn.randint(0,1)
+    test = rn.randint(0,1)
     if test == 1:
-        print("Hallo, Test, Test!")
+        mixer.init()
+        mixer.music.load('boom.wav')
+        mixer.music.play()
+
+
